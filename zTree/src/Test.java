a39383f26ec4c89122f9bb5dc0e691b79aa4c5e9
killer-li <514010914@qq.com>
2015-07-30 00:18:16 +0800

    Update Test.java
    this operation is remote commit

--- zTree/src/Test.java
+++ zTree/src/Test.java
@@ -6,5 +6,6 @@ public class Test {
 		String[] weekDays = {"周二", "周一", "周三", "周四", "周五", "周六", "周日"};
 		Arrays.sort(weekDays);
 		System.out.println(Arrays.toString(weekDays));
+		System.out.println(Arrays[0]);
 	}
 }
