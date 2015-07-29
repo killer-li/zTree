dbfc841d4d3b2660b97a0a9a537b6df57939d0b4
killer-li <514010914@qq.com>
2015-07-29 23:54:46 +0800

    this operation is commit Test.java

---
+++ zTree/src/Test.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+
+public class Test {
+	public static void main(String[] args) {
+		String[] weekDays = {"周二", "周一", "周三", "周四", "周五", "周六", "周日"};
+		Arrays.sort(weekDays);
+		System.out.println(Arrays.toString(weekDays));
+	}
+}
