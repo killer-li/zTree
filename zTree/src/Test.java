538815464277a0db2b08771c903f9e8b9b2d0826
killer-li <514010914@qq.com>
2015-07-30 11:33:55 +0800

    this commit resolve a bug

--- zTree/src/Test.java
+++ zTree/src/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		String[] weekDays = {"周二", "周一", "周三", "周四", "周五", "周六", "周日"};
 		Arrays.sort(weekDays);
+		System.out.println(1431234);
 		System.out.println(Arrays.toString(weekDays));
+		System.out.println();
 	}
 }
